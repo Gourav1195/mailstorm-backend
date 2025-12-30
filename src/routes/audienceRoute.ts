85826170f8d87d8969f95a5dcd5a4dd47f791824
Gourav Modi <gouravmodi1195@gmail.com>
2025-12-30 22:36:09 +0530

    audience-issue-fix

--- src/routes/audienceRoute.ts
+++ src/routes/audienceRoute.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { getAudience, getAudienceCount, estimatedAudience } from "../controllers/audienceController";
+import { getAudience, getAudienceCount, estimateAudience } from "../controllers/audienceController";
 import { authenticateToken } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.get("/", /*authenticateToken,*/ getAudience); 
 router.get("/count", /*authenticateToken,*/ getAudienceCount); 
-router.get("/estimate", /*authenticateToken,*/ estimatedAudience); 
+router.get("/estimate", /*authenticateToken,*/ estimateAudience); 
 export default router;
